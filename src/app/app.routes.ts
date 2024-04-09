447cab196cbb6c7b5feb1b29c358ac2b11627218
bkabhilash0 <bkabhilash0@gmail.com>
2024-04-09 21:25:02 +0530

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {AddProductComponent} from "./components/pages/add-product/add-product.component";
+import {CartComponent} from "./components/pages/cart/cart.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "products/add",
+    component: AddProductComponent
+  },
+  {
+    path: "cart",
+    component: CartComponent
+  },
+];
